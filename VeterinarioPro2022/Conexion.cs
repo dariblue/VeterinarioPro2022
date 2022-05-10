3c42ec194f77387d05a81b90beb8e91c91a4eb58
xp <xp@CETYSDAM00>
2022-05-10 17:46:05 +0200

    Diseño inicial de los form de datos_animal y pedir_cita. Además se ha añadido la base da datos veterinario.

---
+++ VeterinarioPro2022/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace VeterinarioPro2022
+{
+    class Conexion
+    {
+        public MySqlConnection conexion;
+
+        public Conexion()
+        {
+            conexion = new MySqlConnection("Server = 127.0.0.1; Database = veterinario; Uid = root; Pwd =; Port = 3306");
+        }
+    }
+}
