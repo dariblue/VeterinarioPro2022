1248137ed7a7a864326a83859e002c616e14f612
xp <xp@CETYSDAM00>
2022-05-10 16:04:39 +0200

    Prueba

--- VeterinarioPro2022/datosAnimal.cs
+++ VeterinarioPro2022/datosAnimal.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace VeterinarioPro2022
 {
-    public partial class Form1 : Form
+    public partial class datosAnimal : Form
     {
-        public Form1()
+        public datosAnimal()
         {
             InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
